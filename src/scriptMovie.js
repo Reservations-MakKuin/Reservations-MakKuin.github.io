8a9222b779c39f6656ea66a4604e384092764bd1
MiroRuskov <st.ruskov@gmail.com>
2023-07-23 23:03:01 +0300

    d

--- src/scriptMovie.js
+++ src/scriptMovie.js
@@ -516,7 +516,7 @@ function load() {
                        }
                     if(ev.kaparoTime == 'Оставено' || ev.kaparoTime == 'Грабо Ваучер' ){
                         eventDiv.classList.add('event');
-                        eventDiv.innerText = ev.time + "ч." + " " + ev.name + " " + ev.age + "г." + "d";
+                        eventDiv.innerText = ev.time + "ч." + " " + ev.name + " " + ev.age + "г.";
                         
                     }else{
                         eventDiv.classList.add('event');
