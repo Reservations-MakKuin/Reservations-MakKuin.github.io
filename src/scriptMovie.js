7ba5bf24801de75e8e3afe22ce340f4d25b8f2d3
MiroRuskov <st.ruskov@gmail.com>
2023-07-24 23:42:41 +0300

    g

--- src/scriptMovie.js
+++ src/scriptMovie.js
@@ -558,7 +558,7 @@ function load() {
                          eventDiv.style="background-color: #f19407;"
                          eventDiv.innerText = ev.time + "ч." + " " + ev.name + " " + ev.age + "г.";
                     }
-                    else if(ev.cakeDelivery == false && (ev.kaparoTime!== 'Оставено' || ev.kaparoTime !== "Грабо Ваучер")){
+                    else if(ev.cakeDelivery !== false && (ev.kaparoTime!== 'Оставено' || ev.kaparoTime !== "Грабо Ваучер")){
                         eventDiv.classList.add('event');
                         eventDiv.style="background-color: yellow;"
                         eventDiv.innerText = ev.time + "ч." + " " + ev.name + " " + ev.age + "г.";
