1830157ef4ef2c93fdfd4f7e499ac1506737888a
Stanimir Ruskov <79002715+MiroRuskov@users.noreply.github.com>
2026-07-15 23:26:25 +0300

    Update scriptOut.js

--- src/scriptOut.js
+++ src/scriptOut.js
@@ -83,7 +83,7 @@ function openModal(event, date, reservationsArr) {
     // };
 
 
-    if (event.target.className == 'event' && localStorage.getItem('1') != null) {
+    if (event.target.className == 'event' && localStorage.getItem('2') != null) {
 
         newEventModal.style.display = 'none';
         deleteEventModal.style.display = 'block';
@@ -270,7 +270,7 @@ function openModal(event, date, reservationsArr) {
         };
         
     } else {
-        if (event.target.children.length !== 4 && localStorage.getItem('1') != null) {
+        if (event.target.children.length !== 4 && localStorage.getItem('2') != null) {
 
             document.querySelector('#newEventModal h2').textContent = 'Нова Резервация';
             newEventModal.style.display = 'block';
@@ -278,7 +278,7 @@ function openModal(event, date, reservationsArr) {
             document.getElementById('editButton').style.display = 'none';
             document.getElementById('saveButton').style.display = 'inline-block';
             document.getElementById('deleteButton').style.display = 'none';
-        } else if (event.target.children.length !== 4 && localStorage.getItem('1') == null){
+        } else if (event.target.children.length !== 4 && localStorage.getItem('2') == null){
            // return alert('Достигнат Максимум на резервации за ден!');
             document.querySelector('#blockEventModal h2').textContent = 'Не сте логнати!';
             blockEventModal.style.display = 'block';
